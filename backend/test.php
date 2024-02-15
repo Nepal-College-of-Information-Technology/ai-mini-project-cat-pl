8025d2d8450b8a39fe93d90c97eb30cf5c4a49b6
Nischal Dhakal <dnischal11@gmail.com>
2024-02-15 19:58:47 +0545

    GodZer0

--- backend/test.php
+++ backend/test.php
@@ -2,25 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$text = "I am a very good man";
+$text = "I am a man.";
 
  
 $result = shell_exec("/opt/lampp/htdocs/ai-mini-project-cat-pl/venv/bin/python /opt/lampp/htdocs/ai-mini-project-cat-pl/backend/main.py '$text'  2>&1");
 echo $result;
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" c initial-scale=1.0>
-    <title>Test</title>
-</head>
-<body>
-
-    <h1>Result</h1>
-    <p><?php echo $result; ?></p>
-    
-    
-</body>
-</html>
