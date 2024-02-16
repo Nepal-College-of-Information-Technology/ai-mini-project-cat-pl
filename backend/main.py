8c8cb32c3681af0502183ab07b9d5b88353aec32
Nischal Dhakal <dnischal11@gmail.com>
2024-02-16 19:51:09 +0545

    Else if update

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ import sys
 import emoji
 from textblob import TextBlob
 
-
 if len(sys.argv) > 1:
     text = sys.argv[1]
 else:
@@ -15,13 +14,15 @@ def get_sentiment(text):
     sentiment = round(sentiment, 2)
     return sentiment
 
-
 # parse result to float
-
 result = get_sentiment(text)
 
-if result > 0.1:
-    print(result,"  :  positive words !",emoji.emojize(':grinning_face_with_big_eyes:'))
+if result > 0.3:
+    print(result,"  :  very positive words !",emoji.emojize(':grinning_face_with_big_eyes:'))
+elif result > 0.1:
+    print(result,"  :  positive words !",emoji.emojize(':smiling_face_with_smiling_eyes:'))
+elif result < -0.3:
+    print(result,"  :  very negative words !",emoji.emojize(':worried_face:'))
 elif result < -0.1:
     print(result,"  :  negative words !",emoji.emojize(':pensive_face:'))
 else:
