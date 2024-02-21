e40ee00c3bf05f796926c50fd9ef64c295318551
Nischal Dhakal <dnischal11@gmail.com>
2024-02-21 21:32:01 +0545

    empty commit

--- index.php
+++ index.php
@@ -9,74 +9,112 @@
     error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $sentiment_score = 0;
-$sentiment = null;
+$sentiment = "Nothing";
+
+
 // Get the text from the form
 if (isset($_POST['submit'])) {
     $text = $_POST['text'];
 
-    $data = array('text' => $text);
-$data_string = json_encode($data);
 
-// Initialize cURL session
-$ch = curl_init('http://0.0.0.0:8000/sentiment/');
+  $data = array('text' => $text);
+  $data_string = json_encode($data);
 
-// Set cURL options
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data_string))
-);
+  // Initialize cURL session
+  $ch = curl_init('http://0.0.0.0:8000/sentiment/');
 
-// Execute the cURL request
-$result = curl_exec($ch);
+  // Set cURL options
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($data_string))
+  );
 
-// Close cURL session
-curl_close($ch);
+  // Execute the cURL request
+  $result = curl_exec($ch);
 
-// Decode the JSON response
-$response = json_decode($result, true);
+  // Close cURL session
+  curl_close($ch);
 
-// Get the sentiment score from the response
-$sentiment_score = $response['sentiment_score'];
+  // Decode the JSON response
+  $response = json_decode($result, true);
 
-
-
-if($sentiment_score > 0.1){
-    $sentiment = "Positive";
-}else if($sentiment_score < -0.1){
-    $sentiment = "Negative";
-}else{
-    $sentiment = "Neutral";
-}
+  // Get the sentiment score from the response
+  $sentiment_score = $response['sentiment_score'];
 
 
-// Display the sentiment score
+
+  if($sentiment_score > 0.1){
+      $sentiment = "Positive";
+  }else if($sentiment_score < -0.1){
+      $sentiment = "Negative";
+  }else{
+      $sentiment = "Neutral";
+  }
 
 }
-
-// Prepare the data to be sent in the request
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home</title>
-</head>
-<body>
+  </head>
+  <body>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <script
+          src="https://kit.fontawesome.com/919bf9fb4d.js"
+          crossorigin="anonymous"
+        ></script>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <div class="navbar">
+          <a href="#"> Sentiment Analysis </a>
+        </div>
 
-<form action="" method="post">
-    <input type="text" name="text" placeholder="Enter text">
-    <input type="submit" value="Submit" name="submit">
-</form>
+        <div class="body">
+          <div class="text">
+            <form action="" method="post">
+              <textarea
+                name="text"
+                id="text"
+                cols="30"
+                rows="10"
+                placeholder="Enter your text here"
+                class="body-d"
+              ></textarea>
+              <input type="submit" name="submit" value="Submit" class="submit">
+            </form>
+          </div>
 
-<h1>Score : <?php echo $sentiment_score;?></h1>
-<h1>Sentiment : <?php echo $sentiment;?></h1>
-    
-</body>
+          <div class="result">
+            <h1>Result</h1>
+            <div class="result-d">
+              <img src="images/<?php echo $sentiment;?>.jpg" alt="image" />
+              <p>Sentiment: <span id="sentiment">
+                <?php echo $sentiment; ?>
+              </span></p>
+              <p>Sentiment Score: <span id="sentiment-score">
+                <?php echo $sentiment_score; ?>
+              </span></p>
+            </div>
+          </div>
+        </div>
+
+        <script src="script.js"></script>
+      </body>
+    </html>
+  </body>
 </html>
