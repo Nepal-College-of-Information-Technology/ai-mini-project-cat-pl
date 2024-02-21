e2441ee7cd72e28dc2664695de8ca65a95a42ffe
Nischal Dhakal <dnischal11@gmail.com>
2024-02-21 10:35:15 +0545

    session update

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-    // session_start();
-    // if(!isset($_SESSION['email'])){
-    //     header('Location: login.php');
-    // }
+    session_start();
+    if(!isset($_SESSION['email'])){
+        header('Location: login.php');
+    }
 
 
     error_reporting(E_ALL);
